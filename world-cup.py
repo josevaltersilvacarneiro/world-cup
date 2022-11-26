eb24c9b53b28dace42df63c3b787841acd250be1
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-11-26 18:45:19 -0300

    New features were added

--- world-cup.py
+++ world-cup.py
@@ -19,7 +19,7 @@ games = { key : list() for key in groups };
 #------------------------------------------------------------
 ######################### File ##############################
 
-def push() -> None:
+def get() -> None:
 
     global cup, games;
 
@@ -35,7 +35,7 @@ def push() -> None:
     except FileNotFoundError:
         return None;
 
-def get() -> None:
+def push() -> None:
     
     global cup, games;
 
@@ -97,10 +97,14 @@ def register_teams() -> None:
     for i in range(amount_of_teams):
         
         group = get_group();
+
+        if len(cup[group]) == 4:
+            continue;
+
         team = get_team(group);
 
         cup[group].append(team);
-
+    
 def register_game() -> None:
 
     global games;
@@ -134,7 +138,7 @@ def edit_team() -> None:
 
     group = get_group();
 
-    team = get_team(group);
+    team = get_team(group, False);
     new_team = get_team(group);
 
     check = input(f'Você deseja substituir {team} por {new_team}: [S/N]').strip().upper();
@@ -197,13 +201,13 @@ def delete_team() -> None:
     global cup, games;
 
     group = get_group();
-    team = get_team(group);
+    team = get_team(group, False);
     
     check = input(f'Você realmente deseja excluir o {team} do grupo {group}: [S/N]').strip().upper();
 
     if check == 'S':
         
-        for i, game in enumerate(games):
+        for i, game in enumerate(games[group]):
 
             if game[0] == team or game[1] == team:
                 games.pop(i);
@@ -215,13 +219,13 @@ def delete_game() -> None:
     global games;
 
     group = get_group();
-    team_one, team_two = get_team(group), get_team(group);
+    team_one, team_two = get_team(group, False), get_team(group, False);
 
     check = input('Você deseja realmente excluir o jogo {} x {}: [S/N]').strip().upper();
 
     if check == 'S':
 
-        for i, game in enumerate(games):
+        for i, game in enumerate(games[group]):
 
             if (
                     (game[0] == team_one and game[1] == team_two) or 
@@ -242,8 +246,107 @@ def delete_registration() -> None:
     elif option == 2:
         delete_game();
 
-#------------------------------------------------------------
+#-----------------------------------------------------------#
+############### All values were registered ##################
+
+def goal_average_on_group(group : str) -> float:
+    
+    global games;
+
+    number_of_goals : int = 0;
+
+    for game in games[group]:
+
+        number_of_goals += game[2] + game[3];
+
+    return number_of_goals / len(games[group]) if number_of_goals else 0;
+
+def goal_average() -> float:
+    
+    global groups;
+
+    average_of_goals_on_groups : float = 0;
+
+    for group in groups:
+
+        average_of_goals_on_groups += goal_average_on_group(group);
+
+    return average_of_goals_on_groups / len(groups);
+
+def most_goals_in_the_cup() -> list:
+    
+    global games;
+
+    data : list = [];
+
+    for games_on_group in games.values():
+        for game in games_on_group:
+
+            max_game = max(game[2], game[3]);
+            max_data = max(data[2], data[3]) if data else -1;
+
+            if max_game > max_data:
+                data = game;
+    
+    return data[:];
+
+#-----------------------------------------------------------#
 ########################## Main #############################
+
+def all_teams_registered() -> bool:
+
+    for group in cup.values():
+
+        if len(group) < 4:
+            return False;
+
+    return True;
+
+def all_games_registered_on_group(group : str) -> bool:
+
+    global games;
+
+    return len(games[group]) == 6;
+
+def all_games_registered() -> bool:
+
+    global groups;
+
+    for group in groups:
+
+        if not all_games_registered_on_group(group):
+            return False;
+
+    return True;
+
+def all_registered() -> bool:
+
+    return all_teams_registered() and all_games_registered();
+
+#-----------------------------------------------------------#
+########################## Print ############################
+
+def print_goal_average() -> None:
+    
+    goal_average_on_cup = goal_average();
+
+    print(f'A média de gols na copa é {goal_average_on_cup}');
+
+def print_goal_average_by_group() -> None:
+    
+    global groups;
+
+    for group in groups:
+
+        goal_average = goal_average_on_group(group);
+
+        print(f'A média de gols no grupo {group} é {goal_average:.2f}');
+
+def print_team_with_most_goals_in_the_cup() -> None:
+    
+    game = most_goals_in_the_cup();
+
+    print(f'{game[0]} [{game[2]}] vs [{game[3]}] {game[1]}');  
 
 def update() -> None:
 
@@ -261,7 +364,7 @@ def update() -> None:
 
     print('-' * 30);
 
-def print_menu() -> None:
+def print_menu(all_registered : bool = False) -> None:
     
     print('1 → Cadastrar equipes em seus grupos');
     print('2 → Cadastrar jogos');
@@ -269,13 +372,26 @@ def print_menu() -> None:
     print('4 → Excluir cadastro');
     print('5 → Sair');
 
+    if all_registered:
+        print('6 → A média geral de gols na primeira fase');
+        print('7 → A média de gols por jogo em cada grupo');
+        print('8 → Qual seleção fez mais gols em uma partida');
+
 def main() -> int:
 
-    get();                                  # Isso carrega os dados do arquivo, caso o arquivo exista
+    get();                                   # Isso carrega os dados do arquivo, caso o arquivo exista
 
     while True:
-        print_menu();
-        option = get_option(1, 5);
+
+        if all_registered():
+            print_menu(True);
+            option = get_option(3, 8);
+        elif all_teams_registered():
+            print_menu(True);
+            option = get_option(2, 8);
+        else:
+            print_menu();
+            option = get_option(1, 5);
 
         if option == 1:
             register_teams();
@@ -287,8 +403,16 @@ def main() -> int:
             delete_registration();
         elif option == 5:
             break;
+        elif option == 6:
+            print_goal_average();
+        elif option == 7:
+            print_goal_average_by_group();
+        elif option == 8:
+            print_team_with_most_goals_in_the_cup();
 
         update();
+
+    push();                                  # Isso empurra os dados do programa para o arquivo
 
     return 0;
 
