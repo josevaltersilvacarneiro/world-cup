9871582d9e11b937e06f140504c82e8d2e25adb9
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-12-01 18:55:14 -0300

    The file responsible for deleting the teams and the games was created

---
+++ _delete.py
@@ -0,0 +1,43 @@
+"""The functions and procedures of this file are responsible
+for deleting the teams and the games.
+
+"""
+
+def delete_team(cup : dict, games : dict) -> None:
+
+    group : str = get_non_empty_group(cup);
+    team : str = get_team(group, False);
+
+    message : str = f'Do you really want to delete {team} from the group {group}';
+
+    if check(message):
+
+        for i, game in games[group]:
+
+            if game[0] == team or game[1] == team:
+                games[group].pop(i);
+
+        cup[group].remove(team);
+
+def delete_game(cup : dict, games : dict) -> None:
+
+    group : str = get_non_empty_group(games);
+    first_team, second_team = get_two_different_teams(cup, group, False);
+
+    message : str = f'Do you really to delete the game {first_team} vs {second_team}';
+
+    if check(message):
+
+        for i, game in games[group]:
+            
+            if (
+                (game[0] == first_team and game[1] == second_team) or
+                (game[0] == second_team and game[1] == first_team)
+                    ):
+            
+                games[group].pop(i);
+
+                break;
+        else:
+            print('The game that you\'re trying to delete has not been registered yet');
+
