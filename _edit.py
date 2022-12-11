fac520c24e5ab89ba422dcaf4251f614706548b2
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-12-11 12:33:13 -0300

    The program is more user-friendly

--- _edit.py
+++ _edit.py
@@ -13,7 +13,7 @@ from _utils import (
 def edit_team(cup : dict, games : dict) -> None:
 
     group : str = get_non_empty_group(cup);
-    team, new_team = get_team(cup, group, False), get_team(cup, group);
+    team, new_team = get_team(cup, group, False), get_team(cup, group, message='New team: ');
     
     message : str = f'Do you really want to replace {team} by {new_team}';
 
