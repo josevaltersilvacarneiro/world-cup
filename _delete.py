eadc73e5fe5fa7cfb43f51fcce9aed1beeb7ec44
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-12-12 16:48:10 -0300

    Improving the user experience

--- _delete.py
+++ _delete.py
@@ -30,20 +30,20 @@ def delete_game(cup : dict, games : dict) -> None:
     group : str = get_non_empty_group(cup);
     first_team, second_team = get_two_different_teams(cup, group, False);
 
-    message : str = f'Do you really to delete the game {first_team} vs {second_team}';
-
-    if check(message):
-
-        for i, game in enumerate(games[group]):
+    for i, game in enumerate(games[group]):
             
-            if (
+        if  (
                 (game[0] == first_team and game[1] == second_team) or
                 (game[0] == second_team and game[1] == first_team)
-                    ):
-            
+            ):
+
+            message : str = f'Do you really to delete the game {first_team} vs {second_team}';
+
+            if check(message):
                 games[group].pop(i);
 
-                break;
+            break;
+
         else:
             print('The game that you\'re trying to delete has not been registered yet');
 
